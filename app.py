b95702aece9ee8c6132bf5916a19a430683ac1ca
Eric S. Ryan <eric@ericsryan.com>
2023-09-12 13:53:14 -0700

    Add data cleaning functions

--- app.py
+++ app.py
@@ -4,43 +4,84 @@ from models import Base, Product, session, engine
 
 
 def add_csv():
+    """Add contents of inventory.csv to a database"""
     with open('inventory.csv', newline='') as csv_file:
         data = csv.reader(csv_file)
         next(data, None) # Skip the headers
         for row in data:
-            product_in_db = session.query(
-                Product
-            ).filter(
-                Product.product_name==row[0]
-            ).one_or_none()
-
-            if product_in_db == None:
-                product_name = row[0]
-                product_quantity = row[1]
-                product_price = row[2]
-                date_updated = row[3]
-                new_product = Product(
-                    product_name=product_name,
-                    product_quantity=product_quantity,
-                    product_price=product_price,
-                    date_updated=date_updated
-                )
-                session.add(new_product)
+            product_name = clean_name(row[0])
+            product_price = clean_price(row[1])
+            product_quantity = clean_quantity(row[2])
+            date_updated = clean_date(row[3])
+            new_product = Product(
+                product_name=product_name,
+                product_quantity=product_quantity,
+                product_price=product_price,
+                date_updated=date_updated
+            )
+            session.add(new_product)
         session.commit()
 
 
+# Cleaning functions
+def clean_name(name):
+    """Strip quotation marks from string"""
+    return name.strip('"')
+
+
+def clean_price(price):
+    """Strip dollar sign from price and convert to integer"""
+    try:
+        price_float = float(price.strip('$'))
+        price = int(price_float * 100)
+    except ValueError:
+        input("""
+***** PRICE ERROR *****
+The price should only include numbers 1-10 and the '.' symbol.
+Press [Enter] to continue...
+""")
+    else:
+        return price
+    
+
+def clean_quantity(quantity):
+    """Convert quantity into an integer"""
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        input("""
+***** PRICE ERROR *****
+The price should only include numbers 1-10 and the '.' symbol.
+Press [Enter] to continue...
+""")
+    else:
+        return quantity
+    
+
+def clean_date(date):
+    """Convert date string into a date object"""
+    try:
+        date = datetime.datetime.strptime(date, '%m/%d/%Y')
+    except ValueError:
+        input("""
+***** DATE ERROR *****
+The date should be formatted MM/DD/YYYY.
+Press [Enter] to continue...
+""")
+    else:
+        return date
+
+
+
 def app():
+    """Application logic"""
     print("App")
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    if not session.query(Product).one_or_none():
-        print("Setting up the database...")
+    if session.query(Product).count() == 0: # Add products to database if empty
         add_csv()
-        products = session.query(Product)
-        for product in products:
-            print(product.product_name)
     app()
 
     
